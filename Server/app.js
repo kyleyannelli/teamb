7cbbd1cf208165baac5c0aa1741e9027bd380f6a
kyleyannelli <kyannell@oswego.edu>
2021-09-16 12:55:51 -0400

    added web playback functionality
    script linked in player html
    web playback js added in app.js

--- Server/app.js
+++ Server/app.js
@@ -36,6 +36,7 @@ function onPageLoad(){
         else {
             // we have an access token so present device section
             document.getElementById("deviceSection").style.display = 'block';
+
             refreshDevices();
             refreshPlaylists();
             currentlyPlaying();
@@ -43,6 +44,7 @@ function onPageLoad(){
     }
     refreshRadioButtons();
 }
+
 
 //removes everything from storage and logs out
 function removeAll() {
@@ -369,4 +371,37 @@ function addRadioButton(item, index){
     node.innerText = index;
     node.onclick = function() { onRadioButton( item.deviceId, item.playlistId ) };
     document.getElementById("radioButtons").appendChild(node);
+}
+
+// Set up the Web Playback SDK
+
+window.onSpotifyPlayerAPIReady = () => {
+    const player = new Spotify.Player({
+        name: 'Web Playback SDK Template',
+        getOAuthToken: cb => { cb(access_token); }
+    });
+
+    // Error handling
+    player.on('initialization_error', e => console.error(e));
+    player.on('authentication_error', e => console.error(e));
+    player.on('account_error', e => console.error(e));
+    player.on('playback_error', e => console.error(e));
+
+    // Playback status updates
+    player.on('player_state_changed', state => {
+        console.log(state)
+        $('#current-track').attr('src', state.track_window.current_track.album.images[0].url);
+        $('#current-track-name').text(state.track_window.current_track.name);
+    });
+
+    // Ready
+    player.on('ready', data => {
+        console.log('Ready with Device ID', data.device_id);
+
+        // Play a track using our new device ID
+        play(data.device_id);
+    });
+
+    // Connect to the player!
+    player.connect();
 }
